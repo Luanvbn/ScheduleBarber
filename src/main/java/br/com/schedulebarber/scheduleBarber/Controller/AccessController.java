a7c65fad8e271e5cfec0cdd4a225973f50b90e80
Luanvbn <luanvbn@gmail.com>
2023-05-12 15:52:58 -0300

    Finalização da criação do endpoint de acesso para adm

---
+++ src/main/java/br/com/schedulebarber/scheduleBarber/Controller/AccessController.java
@@ -0,0 +1,46 @@
+package br.com.schedulebarber.scheduleBarber.Controller;
+
+
+import br.com.schedulebarber.scheduleBarber.Exception.AccessAlreadyExistsException;
+import br.com.schedulebarber.scheduleBarber.Exception.AccessNotExistsException;
+import br.com.schedulebarber.scheduleBarber.Exception.BarberNotExistsException;
+import br.com.schedulebarber.scheduleBarber.Model.Access;
+import br.com.schedulebarber.scheduleBarber.Model.Barber;
+import br.com.schedulebarber.scheduleBarber.Service.AccessService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/access")
+public class AccessController {
+
+    @Autowired
+    private AccessService accessService;
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<Optional<Access>> findById(@PathVariable("id") Long id) throws AccessNotExistsException {
+        Optional<Access> access = accessService.findAccessById(id);
+        return ResponseEntity.ok(access);
+    }
+
+    @PostMapping("/createAccess")
+    public ResponseEntity<?> saveAccess (@RequestBody Access access) throws AccessAlreadyExistsException {
+        Access savedAccess = accessService.createAdmin(access);
+        return ResponseEntity.ok(savedAccess);
+    }
+
+    @PutMapping ("/update/{id}")
+    public ResponseEntity<Access> updateAccess(@PathVariable Long id, @RequestBody Access body) throws AccessNotExistsException {
+        Access access = accessService.updateAccess(id, body);
+        return ResponseEntity.ok().body(access);
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<Access> deleteAccess (@PathVariable Long id) throws AccessNotExistsException {
+        Access access = accessService.deleteAccess(id);
+        return ResponseEntity.ok().body(access);
+    }
+}
--- src/main/java/br/com/schedulebarber/scheduleBarber/Service/AccessService.java
+++ src/main/java/br/com/schedulebarber/scheduleBarber/Service/AccessService.java
@@ -3,14 +3,18 @@ package br.com.schedulebarber.scheduleBarber.Service;
 import br.com.schedulebarber.scheduleBarber.Exception.AccessAlreadyExistsException;
 import br.com.schedulebarber.scheduleBarber.Exception.AccessNotExistsException;
 import br.com.schedulebarber.scheduleBarber.Exception.BarberNotExistsException;
+import br.com.schedulebarber.scheduleBarber.Exception.ClientNotExistsException;
 import br.com.schedulebarber.scheduleBarber.Model.Access;
 import br.com.schedulebarber.scheduleBarber.Model.Barber;
+import br.com.schedulebarber.scheduleBarber.Model.Client;
 import br.com.schedulebarber.scheduleBarber.Repository.AccessRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+
+import static br.com.schedulebarber.scheduleBarber.Util.RemovedAcent.removerAcento;
 
 @Service
 public class AccessService {
@@ -22,7 +26,7 @@ public class AccessService {
         if (accessRepository.existsByEmail(access.getEmail())) {
             throw new AccessAlreadyExistsException();
         } else {
-            Access accessSaved = new Access();
+            Access accessSaved = access;
             accessSaved.setRole("ADMIN");
             return accessRepository.save(accessSaved);
         }
@@ -36,4 +40,32 @@ public class AccessService {
             throw new AccessNotExistsException();
         }
     }
+
+    public Access updateAccess(Long id, Access access) throws AccessNotExistsException {
+        Optional<Access> accessOptional = accessRepository.findById(id);
+        Access existingAccess = accessOptional.orElseThrow(AccessNotExistsException::new);
+
+        if (access.getEmail() != null) {
+            existingAccess.setEmail(removerAcento(access.getEmail()));
+        }
+        if (access.getPassword() != null) {
+            existingAccess.setPassword(access.getPassword());
+        }
+
+        Access accessSave = accessRepository.save(existingAccess);
+        return accessRepository.save(accessSave);
+    }
+
+    public Access deleteAccess (Long id) throws AccessNotExistsException {
+        Optional<Access> accessOptional = accessRepository.findById(id);
+        if(accessOptional.isPresent()) {
+            Access existingAccess = accessOptional.get();
+            accessRepository.deleteById(id);
+            return existingAccess;
+        } else {
+            throw new AccessNotExistsException();
+        }
+    }
+
+
 }
